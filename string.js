eb19d47db39ba08cc6379bbfcf19cc196983f48e
Your Name <you@example.com>
2020-11-16 16:30:46 +0600

    third commit

--- string.js
+++ string.js
@@ -3,11 +3,20 @@
 // var b = 20;
 // console.log(a);
 // console.log(b);
-var x= {
-    name : "jhon"
-};
-var y = x;
-y.name = "David";
-console.log(y.name);
+// var x= {
+//     name : "jhon"
+// };
+// var y = x;
+// y.name = "David";
+// console.log(y.name);
 // console.log(x);
 // console.log(x.name);
+function vehical (name,maker,engine){
+    this.name=name;
+    this.maker=maker;
+    this.engine= engine;
+}
+let car = new vehical("GT", "BMW", "2000cc");
+console.log(car.name);
+console.log(car.engine);
+
