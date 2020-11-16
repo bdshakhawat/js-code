1dd2266d12c78b20417718f1bd28b13b344aa0a0
Your Name <you@example.com>
2020-11-16 16:05:50 +0600

    second commit

--- string.js
+++ string.js
@@ -1,3 +1,13 @@
-var a = 10;
-var b = 20;
-console.log(b);
+// var a = 10;
+// var b=a;
+// var b = 20;
+// console.log(a);
+// console.log(b);
+var x= {
+    name : "jhon"
+};
+var y = x;
+y.name = "David";
+console.log(y.name);
+// console.log(x);
+// console.log(x.name);
