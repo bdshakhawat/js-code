a1c5da90291d33d8bafec6cc3d34bd7a10f3526c
Your Name <you@example.com>
2020-12-01 10:57:05 +0600

    nine commit

---
+++ await.js
@@ -0,0 +1,19 @@
+/* A simple function returning hello and out put will be only "Hello"
+function friendyFunction (){
+    return "Hello";
+}
+console.log(friendyFunction());
+ output: Hello.*/
+ /* Using async  we will get a promise return
+async function friendyFunction (){
+    return "Hello";
+}
+console.log(friendyFunction());
+output: Promise { 'Hello' } */
+
+
+/*function friendlyFunction() {
+    return Promise.resolve("hello");
+}
+console.log(friendlyFunction());
+Promise { 'hello' } */
