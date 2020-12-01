80dac9cb4b0142a7cac8e4220feb18c9a3e6d9a8
Your Name <you@example.com>
2020-12-01 14:22:17 +0600

    tenth

--- await.js
+++ await.js
@@ -17,3 +17,62 @@ output: Promise { 'Hello' } */
 }
 console.log(friendlyFunction());
 Promise { 'hello' } */
+// converting the promise into async await function to avoid using .then complexcity. In earlier case we have two promises addTocalandar and 
+//  meting then make a chain of promise to add them together.Here meting promise is returning a metingDetails 
+const hasMeting = true;
+// const hasMeting = false;
+const meting = new Promise((resolve,reject)=> {
+    if(!hasMeting){
+        const metingDetails = {
+            name : "Technical meting",
+            location : "google meet" ,
+            time : "10:00 pm"
+        }
+        resolve(metingDetails);
+    } else {
+        reject (new Error("Meting already sceduled"));
+    }
+
+}) 
+
+
+const addToCalander = (metingDetails)=>{
+    
+        const calendar = `${metingDetails.name} has been sceduled on ${metingDetails.location} at ${metingDetails.time}`;
+       return Promise.resolve(calendar);
+
+};
+
+// meting
+
+    
+//     .then(addToCalander)
+//     .then((res)=>{
+//         console.log(JSON.stringify(res));
+//     })
+//     .catch((err)=>{
+//         console.log(err.message); 
+
+//     }) 
+// async function myMeeting() {
+//      const metingDetails = await meting;
+//     const calandar = await addToCalander(metingDetails);
+//     console.log(calandar);
+    
+       
+// }
+// myMeeting();
+
+// now to handle reject function or catch we need to use try and catch function 
+    async function myMeeting() {
+        try{
+        const metingDetails = await meting;
+        const calandar = await addToCalander(metingDetails);
+        console.log(calandar);
+        }catch{
+            console.log(`Something wrong happen`);
+        }
+        
+    }
+    myMeeting();
+    
