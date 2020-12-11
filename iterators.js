b0eccd678b789eba8cae6940ece88e3fd69727fd
Your Name <you@example.com>
2020-12-11 23:55:09 +0600

    othr commit

--- iterators.js
+++ iterators.js
@@ -120,13 +120,99 @@ __proto__: Object.     */
  console.log(iterator.next());
  console.log(iterator.next());
  console.log(iterator.next());
+ console.log(iterator.next());
 //  output: {value: 1, done: false} this means it is giving an object whosw properties are value and done.It means it is 
 //  donating us the iterator as an object and we can use it as we like.When i was using for....of by default it is calling
 // this symbol.iterator function again and again but javascript is giving me this iterator saperately and if i want i can 
 // bring it out and can change the behaviour of the for...of by using this symbol.iterator function.
 // so, if i call the 'next()' function first time then i will get the first value of array = [1,2,3] as output like
 // {value: 1, done: false} if i call it second time and third time then i will get the second and third value of 
-array = [1,2,3] as output like// {value: 1, done: false}, {value: 2, done: false}, {value: 3, done: false     */
+array = [1,2,3] as output like// {value: 1, done: false}, {value: 2, done: false}, {value: 3, done: false . Now if
+i call it fourth time then the output will be : {value:undefined, done:True} that means there is no item to assign value
+and done "true" means the iteration is complete. 
+Now if i want i can return another object by the next() function then i can change the for.....of */
+
+
+//***** Iterator Protocol****
+// 1. It must be an javascript object. 02. It needs to implement a next() method and have to return an object with done and value property.
+// Now, we know arry comes from big Array and inside this array there is a symbol.iterator()function inserted by javascript
+// now if we create our own data type like array=[1,2,3] and insert the function symbol.iterator() in it and if it 
+// symbol.iterator() is a function and it has an implementation of next() function in it and can 
+//  retun an object then my own created data type is also a part of iterator protocol.
+// Now if we want to create a custom iterator then we have to write a next() function by ourselves which will return
+//  an object with two properties. one is 'value' which will take the elements from iteratable object or any js value by maintaining 
+// the order and second property will be "done" which will take either fasle or true. If the operation is done then
+// it take true only one time after finishing the operation and other than this as long as the operation will be 
+// continued it will take a false value every time.Here i have the liberty that i can take the third value first then ninth
+// value and whatever value i need.In case of for loop i need to think about the whole array rather with the iterator i can
+// think only about a single element of the array to use according to my requirement.
+// now lets see an example of string
+// "javascript"[Symbol.iterator]().next()
+// output: {value: "j", done: false}
+
+
+
+// *******Lets make a custom iterator**********spread operator works only in iterator.The function of spread operator 
+// is to bring out the elements from any iterable by spreading.For example:
+
+
+/*         console.log([..."Hello"]);
+// (5) ["H", "e", "l", "l", "o"]
+0: "H"
+1: "e"
+2: "l"
+3: "l"
+4: "o"
+length: 5
+__proto__: Array(0)  spreaded all the letters from the string with their index number and gives an array of letterw. */
+// In this case spread operator call the symbol.iterator() and bring out all the elements from the iterable string but 
+// in case of number it will give error because number is not iterable. so spread operator is not applicable for non-iterable object.
+
+// Now lets change this global behaviour of iterator and will create a custom iterator. Now i want that when 
+// spread operatoe will go through the string and return letters but then we will another thing instead of letter.
+// now to do this we need to change or overwrite the symbol.iterator() inside the prototype of the big string or new string or object
+// representation of string.Because to iterate spread operator use this method . If i change this sybmbol.iterator() then the 
+// behaviour of the 'spread operator' will be changed.
+// To do this write big string of prototype of symbol.iterator menans  String.prototype[symbol.iterator]. It is a function.
+// so write this function by my own.
+String.prototype[Symbol.iterator] = function() {
+
+    // have to follow the iterator protocol 1. retunr an object
+    let count = this.length;
+    // here i will get the length of the string.
+    return {
+        // 2. must have the implementation of next() function
+        next(){
+            // it will return {value:"js", done:true/false}. now i want that in every iteration i will get the same value
+            // js but i will send "Hello" and it consist five letters and will be iterate five times but will return js five times 
+            // instead of H  E  L  L  O it will give output   js  js   js   js  js.
+            // here i will check how long this counting will be continued.It will be continued till the value is greater than zero and will return
+            // done: false/true value:js
+            if(count>0){
+                count --;
+                return  {done:false, value:"js"}        
+                
+            }
+            return {done:true};
+            
+
+
+        }
+
+    }
+
+}
+
+
+console.log(..."Hello");
+// js js js js js
+
+
+
+
+
+
+
 
 
 
