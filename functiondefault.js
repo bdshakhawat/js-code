7f49ed37c6199d1ff66040b05f45fee6b6a98bf7
Your Name <you@example.com>
2020-12-26 09:44:49 +0600

    second commit

---
+++ functiondefault.js
@@ -0,0 +1,18 @@
+// function default parameter value
+function myFunc (x=10) {
+    return x;
+
+}
+// console.log(myFunc());
+// output: undefined
+// console.log(myFunc(5));
+// output: 5
+// console.log(myFunc(5));
+//  output: 5
+// console.log(myFunc());
+// output: 10
+// console.log(myFunc(null));
+// output: 10
+console.log(myFunc(undefined));
+// output: 10
+
